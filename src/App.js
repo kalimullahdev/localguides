5e8b6927aeff869a2935a34e7585a53dd1a2715d
kalimullahdev <kalimullahdev@gmail.com>
2021-03-07 10:25:54 +0500

    Added Routing Feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import Register from './pages/Register/Register';
 import SignIn from './pages/SignIn/SignIn';
 import Slider from './pages/Slider/Slider';
@@ -7,11 +14,19 @@ import Slider from './pages/Slider/Slider';
 function App() {
   return (
     <div className="App">
-      <h1>
-        <Slider/>
-        <Register/>
-        <SignIn/>
-      </h1>
+      <Router>
+        <Switch>
+          <Route path="/slider">
+            <Slider/>
+          </Route>
+          <Route path="/register">
+             <Register/>
+          </Route>
+          <Route path="/signin">
+            <SignIn/>
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
